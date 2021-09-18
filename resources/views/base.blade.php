a0ab89b56bb4e2f17eaf83beb8fe0e10d9a2aebe
William Lima <williamlims@hotmail.com>
2021-09-18 20:29:39 -0300

    Setting assets

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--Import Google Icon Font-->
+        <link href="{{asset('fonts/icon.css')}}" rel="stylesheet">
+        <link href="{{asset('css/materialize.min.css')}}"  rel="stylesheet" media="screen,projection">
+        <title>Cru</title>
+    </head>
+    <body>
+
+
+
+        <script src="{{asset('js/materialize.min.js')}}"></script>
+    </body>
+</html>
